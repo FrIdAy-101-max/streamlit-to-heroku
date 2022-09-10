46f530e2966a5266683d222a13a6a7b018e8d5c1
FrIdAy-101-max <77102491+FrIdAy-101-max@users.noreply.github.com>
2022-09-10 15:35:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.title("Feed")
 
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
-model2 = tf.keras.models.load_model('C:\\Users\\nithe\\PycharmProjects\\pstreamlit\\model1.h5')
+model2 = tf.keras.models.load_model('model1.h5')
 
 while True:
     _, frame1 = camera.read()
